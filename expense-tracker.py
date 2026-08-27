20b7c9f24a410ad6121cb9b24f461417b3ad160f
abdussamadcodes <abdus.samad.codes@gmail.com>
2026-08-27 08:36:44 +0600

    add getValidChoice() function and create empty list for storing expenses info

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,1 +1,17 @@
+import json
+import datetime
+import time
 
+
+def getValidChoice():
+    choice = int()
+    while True:
+        try:
+            choice = int(input("enter your choice: "))
+        except:
+            print("wrong choice. input should be string.")
+        else:
+            return choice
+
+
+choice = getValidChoice()
