1e9a9d9f755a095fbc5977a16c105cb5092e548c
abdussamadcodes <abdus.samad.codes@gmail.com>
2026-08-27 14:32:17 +0600

    modify getvalidchoice() function to get correct choice and make correct structure for main menu

--- expense-tracker.py
+++ expense-tracker.py
@@ -2,8 +2,10 @@ import json
 import datetime
 import time
 
+expenses = []
 
-def getValidChoice():
+
+def getValidChoice(index):
     choice = int()
     while True:
         try:
@@ -11,7 +13,24 @@ def getValidChoice():
         except:
             print("wrong choice. input should be string.")
         else:
-            return choice
+            if choice in range(1, index):
+                return choice
+            else:
+                print(f"wrong choice. input should be between 1 to {index-1}")
 
 
-choice = getValidChoice()
+def mainMenu():
+    while True:
+        print("===== Expense Tracker =====")
+        print("""1.Add expense
+2.View all expense
+3.Search expenses
+4.Filter by category
+5.Show total spending
+6.Export data
+7.Exit
+""")
+        choice = getValidChoice(8)
+
+
+mainMenu()
